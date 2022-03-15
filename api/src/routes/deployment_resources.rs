57d4c843095279f7b78f3f7b1ed9c59bac739e84
Zohar Zilberman <popen2@gmail.com>
2022-03-15 18:59:34 +0200

    Fix /api/v1/deployment-resources/:id not calling without_sensitive_props

--- api/src/routes/deployment_resources.rs
+++ api/src/routes/deployment_resources.rs
@@ -24,7 +24,12 @@ async fn get_all() -> ApiResult {
 
 #[actix_web::get("/{id}")]
 async fn get(id: web::Path<Uuid>) -> ApiResult {
-    Ok(HttpResponse::Ok().json(DeploymentResource::find(id.into_inner()).await?))
+    Ok(HttpResponse::Ok().json(
+        DeploymentResource::find(id.into_inner())
+            .await?
+            .without_sensitive_props()
+            .await?,
+    ))
 }
 
 #[actix_web::post("")]
