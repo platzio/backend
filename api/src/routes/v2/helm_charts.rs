9eeb2eeb9502178a6c15dbdd44ff0641bb1774b4
Zohar Zilberman <popen2@gmail.com>
2022-07-02 15:52:47 +0300

    Add in_use query parameter to /api/v2/helm-charts (fixed #26)

--- api/src/routes/v2/helm_charts.rs
+++ api/src/routes/v2/helm_charts.rs
@@ -1,11 +1,16 @@
 use crate::result::ApiResult;
 use actix_web::{web, HttpResponse};
 use platz_auth::ApiIdentity;
-use platz_db::{HelmChart, HelmChartFilters};
+use platz_db::{HelmChart, HelmChartExtraFilters, HelmChartFilters};
 use uuid::Uuid;
 
-async fn get_all(_identity: ApiIdentity, filters: web::Query<HelmChartFilters>) -> ApiResult {
-    Ok(HttpResponse::Ok().json(HelmChart::all_filtered(filters.into_inner()).await?))
+async fn get_all(
+    _identity: ApiIdentity,
+    filters: web::Query<HelmChartFilters>,
+    extra_filters: web::Query<HelmChartExtraFilters>,
+) -> ApiResult {
+    Ok(HttpResponse::Ok()
+        .json(HelmChart::all_filtered(filters.into_inner(), extra_filters.into_inner()).await?))
 }
 
 async fn get(_identity: ApiIdentity, id: web::Path<Uuid>) -> ApiResult {
