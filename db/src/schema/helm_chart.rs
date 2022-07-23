e9cedcc72789c71ddd43687b88f3b89d0d282bf1
Zohar Zilberman <popen2@gmail.com>
2022-07-23 18:44:10 +0300

    Allow filtering charts by registry kind

--- db/src/schema/helm_chart.rs
+++ db/src/schema/helm_chart.rs
@@ -60,6 +60,7 @@ pub struct HelmChart {
 #[derive(Debug, Deserialize)]
 pub struct HelmChartExtraFilters {
     in_use: Option<bool>,
+    kind: Option<String>,
 }
 
 #[derive(QueryId)]
@@ -114,6 +115,47 @@ where
     }
 }
 
+#[derive(QueryId)]
+struct HelmChartsByKind<T> {
+    query: T,
+    kind: String,
+}
+
+impl<T> Query for HelmChartsByKind<T>
+where
+    T: Query,
+{
+    type SqlType = T::SqlType;
+}
+
+impl<T> RunQueryDsl<PgConnection> for HelmChartsByKind<T> {}
+impl<T> QueryDsl for HelmChartsByKind<T> {}
+
+impl<T> QueryFragment<Pg> for HelmChartsByKind<T>
+where
+    T: QueryFragment<Pg>,
+{
+    fn walk_ast(&self, mut pass: AstPass<Pg>) -> QueryResult<()> {
+        pass.push_sql(
+            "
+            SELECT \"helm_charts\".* FROM (",
+        );
+        self.query.walk_ast(pass.reborrow())?;
+        pass.push_sql(
+            ") AS helm_charts
+            INNER JOIN
+                helm_registries
+            ON
+                helm_charts.helm_registry_id = helm_registries.id
+            WHERE
+                helm_registries.kind = 
+            ",
+        );
+        pass.push_bind_param::<diesel::sql_types::VarChar, String>(&self.kind)?;
+        Ok(())
+    }
+}
+
 impl HelmChart {
     pub async fn all() -> DbResult<Vec<Self>> {
         Ok(helm_charts::table.get_results_async(pool()).await?)
@@ -129,16 +171,29 @@ impl HelmChart {
         let per_page = filters.per_page.unwrap_or(DEFAULT_PAGE_SIZE);
 
         let (items, num_total) = tokio::task::spawn_blocking(move || {
-            if extra_filters.in_use.unwrap_or_default() {
-                InUseHelmCharts(Self::filter(&filters))
+            match (extra_filters.in_use.unwrap_or_default(), extra_filters.kind) {
+                (true, Some(kind)) => InUseHelmCharts(HelmChartsByKind {
+                    query: Self::filter(&filters),
+                    kind,
+                })
+                .paginate(Some(page))
+                .per_page(Some(per_page))
+                .load_and_count::<Self>(&conn),
+                (true, None) => InUseHelmCharts(Self::filter(&filters))
                     .paginate(Some(page))
                     .per_page(Some(per_page))
-                    .load_and_count::<Self>(&conn)
-            } else {
-                Self::filter(&filters)
+                    .load_and_count::<Self>(&conn),
+                (false, Some(kind)) => HelmChartsByKind {
+                    query: Self::filter(&filters),
+                    kind,
+                }
+                .paginate(Some(page))
+                .per_page(Some(per_page))
+                .load_and_count::<Self>(&conn),
+                (false, None) => Self::filter(&filters)
                     .paginate(Some(page))
                     .per_page(Some(per_page))
-                    .load_and_count::<Self>(&conn)
+                    .load_and_count::<Self>(&conn),
             }
         })
         .await
