0da6ab4461f622e9bdb377122d99f78d79d5c1ce
Zohar Zilberman <popen2@gmail.com>
2023-02-01 10:39:41 +0200

    Upgrade base64, encode user token with URL safe no pad

--- auth/src/user_token.rs
+++ auth/src/user_token.rs
@@ -1,4 +1,5 @@
 use crate::error::AuthError;
+use base64::prelude::*;
 use platz_db::UserToken;
 use rand::random;
 use sha2::{Digest, Sha256};
@@ -18,16 +19,21 @@ fn calculate_secret_hash(secret: &str) -> String {
     let mut hasher = Sha256::new();
     hasher.update(secret);
     let secret_sha256 = hasher.finalize();
-    base64::encode(secret_sha256)
+    BASE64_URL_SAFE_NO_PAD.encode(secret_sha256)
 }
 
 pub async fn generate_user_token() -> Result<UserTokenInfo, AuthError> {
     let token_id = Uuid::new_v4();
-    let secret = base64::encode(spawn_blocking(random::<[u8; USER_TOKEN_SECRET_BYTES]>).await?);
+    let secret = BASE64_URL_SAFE_NO_PAD
+        .encode(spawn_blocking(random::<[u8; USER_TOKEN_SECRET_BYTES]>).await?);
     let secret_hash = calculate_secret_hash(&secret);
     Ok(UserTokenInfo {
         secret_hash,
-        token_value: format!("{}.{}", &base64::encode(token_id.as_bytes()), secret),
+        token_value: format!(
+            "{}.{}",
+            &BASE64_URL_SAFE_NO_PAD.encode(token_id.as_bytes()),
+            secret
+        ),
         token_id,
     })
 }
@@ -35,7 +41,8 @@ pub async fn generate_user_token() -> Result<UserTokenInfo, AuthError> {
 pub(crate) async fn validate_user_token(user_token: String) -> Result<UserToken, AuthError> {
     let Some((token_id, secret)) = user_token.split_once('.') else { return Err(AuthError::UserTokenAuthenticationError("Invalid token".to_string())); };
     let token_uuid = Uuid::from_bytes(
-        base64::decode(token_id.as_bytes())
+        BASE64_URL_SAFE_NO_PAD
+            .decode(token_id.as_bytes())
             .ok()
             .and_then(|decoded| decoded.as_slice().try_into().ok())
             .ok_or_else(|| AuthError::UserTokenAuthenticationError("Invalid token".to_string()))?,
