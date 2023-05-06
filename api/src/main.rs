7529bd8bb74b4e3394ca6faf9cf2fd4fe680d2e8
Zohar Zilberman <popen2@gmail.com>
2023-05-06 10:59:38 +0300

    Add run command to api service

--- api/src/main.rs
+++ api/src/main.rs
@@ -9,40 +9,50 @@ mod serde_utils;
 mod server;
 
 #[derive(Parser)]
-struct Config {
-    #[clap(flatten)]
-    server: server::Config,
-
-    #[clap(flatten)]
-    auth: platz_auth::Config,
-
-    #[clap(long, default_value = "5secs")]
-    prometheus_update_interval: humantime::Duration,
+enum Command {
+    #[command(name = "run")]
+    Run {
+        #[clap(flatten)]
+        server_config: server::Config,
+        #[clap(flatten)]
+        auth_config: platz_auth::Config,
+        #[clap(long, default_value = "5secs")]
+        prometheus_update_interval: humantime::Duration,
+    },
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
-    let config = Config::parse();
-    env_logger::Builder::new()
-        .filter(None, log::LevelFilter::Debug)
-        .init();
+    let command = Command::parse();
 
-    init_db(true).await?;
+    match command {
+        Command::Run {
+            server_config,
+            auth_config,
+            prometheus_update_interval,
+        } => {
+            env_logger::Builder::new()
+                .filter(None, log::LevelFilter::Debug)
+                .init();
 
-    let oidc_login = config.auth.into();
+            init_db(true).await?;
 
-    tokio::select! {
-        result = crate::routes::metrics::update_metrics_task(
-            config.prometheus_update_interval.into(),
-            ) => {
-            warn!("Prometheus metrics finished");
-            result
-        }
+            let oidc_login = auth_config.into();
 
-        result = server::serve(config.server, oidc_login) => {
-            warn!("API server finished");
-            result
+            tokio::select! {
+                result = crate::routes::metrics::update_metrics_task(
+                    prometheus_update_interval.into(),
+                    ) => {
+                    warn!("Prometheus metrics finished");
+                    result
+                }
+
+                result = server::serve(server_config, oidc_login) => {
+                    warn!("API server finished");
+                    result
+                }
+            }
         }
     }
 }
