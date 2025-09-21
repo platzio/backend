05cf0d2fbf623786b33b7c7a933a1dd71be307c1
Zohar Zilberman <popen2@gmail.com>
2025-09-21 10:08:34 +0300

    Allow getting database config from regular Postgres environment variables

--- db/src/config.rs
+++ db/src/config.rs
@@ -1,5 +1,32 @@
+use clap::Parser;
 use std::env;
 
+#[derive(Parser)]
+struct DatabaseConfig {
+    #[arg(env = "PGHOST")]
+    pg_host: String,
+    #[arg(env = "PGPORT")]
+    pg_port: u16,
+    #[arg(env = "PGUSER")]
+    pg_user: String,
+    #[arg(env = "PGPASSWORD", hide_env_values = true)]
+    pg_password: String,
+    #[arg(env = "PGDATABASE")]
+    pg_database: String,
+}
+
 pub fn database_url() -> String {
-    env::var("DATABASE_URL").expect("DATABASE_URL is not defined")
+    // Legacy database URL, will be removed in a future version
+    if let Ok(url) = env::var("DATABASE_URL") {
+        return url;
+    }
+
+    let DatabaseConfig {
+        pg_host,
+        pg_port,
+        pg_user,
+        pg_password,
+        pg_database,
+    } = DatabaseConfig::parse();
+    format!("postgres://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}")
 }
